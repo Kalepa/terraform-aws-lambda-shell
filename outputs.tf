8156bf9490698b3e434336efc6bf024c8ee7bc6b
Kyle Kotowick <kotowick@invictonlabs.com>
2021-10-03 10:47:08 -0400

    Revert output changed, since they're not really needed

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-output "lambda_shell_arn" {
-  description = "The ARN of the Lambda function. Used for lambda-shell-resource and lambda-shell-data modules >= v0.2.0."
-  value = module.shell_lambda.lambda.arn
-}
-
 output "invicton_labs_lambda_shell_arn" {
-  description = "The ARN of the Lambda function. Used for lambda-shell-resource and lambda-shell-data modules < v0.2.0 (backwards compatibility)."
+  description = "The ARN of the Lambda function. The output is oddly named because it's used to ensure that the correct module is passed into the lambda-shell-resource and lambda-shell-data modules."
   value = module.shell_lambda.lambda.arn
 }
